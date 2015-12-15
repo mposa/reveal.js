0d5c64da02ff98a56dae9e1f9b17529160f0ebfb
mposa <michael.a.posa@gmail.com>
2015-12-15 15:22:48 -0500

    Fixed issue with overview.

--- js/reveal.js
+++ js/reveal.js
@@ -1774,8 +1774,8 @@
 	function layoutOverview() {
 
 		var margin = 70;
-		var slideWidth = config.width + margin,
-			slideHeight = config.height + margin;
+		var slideWidth = Number(config.width) + margin,
+			slideHeight = Number(config.height) + margin;
 
 		// Reverse in RTL mode
 		if( config.rtl ) {
@@ -1817,8 +1817,8 @@
 	function updateOverview() {
 
 		var margin = 70;
-		var slideWidth = config.width + margin,
-			slideHeight = config.height + margin;
+		var slideWidth = Number(config.width) + margin,
+			slideHeight = Number(config.height) + margin;
 
 		// Reverse in RTL mode
 		if( config.rtl ) {
